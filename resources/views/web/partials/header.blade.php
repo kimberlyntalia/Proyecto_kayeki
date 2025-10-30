e6e109c7b0eb23fb15a32f21c0cc085b000876da
kimberlyntalia <kimberlynatalias21@gmail.com>
2025-10-29 19:07:37 -0500

    proyecto inicial

---
+++ resources/views/web/partials/header.blade.php
@@ -0,0 +1,8 @@
+<header class="bg-dark py-5">
+    <div class="container px-4 px-lg-5 my-5">
+        <div class="text-center text-white">
+            <h1 class="display-4 fw-bolder">Tienda Online</h1>
+            <p class="lead fw-normal text-white-50 mb-0">Tienda de productos tecnológicos</p>
+        </div>
+    </div>
+</header>
