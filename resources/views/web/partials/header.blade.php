eb5e2a69e2fb9b8c20765c18d0d6fe5fe0f26fb1
kimberlyntalia <kimberlynatalias21@gmail.com>
2025-11-11 21:08:20 -0500

    se agregaron nuevas imagenes y nuevo encabezado

--- resources/views/web/partials/header.blade.php
+++ resources/views/web/partials/header.blade.php
@@ -1,9 +1,23 @@
-<header class="bg-ks py-5">
-    <div class="container px-4 px-lg-5 my-5">
+<header class="bg-ks py-5 encabezado-animado">
+    <div class="container px-4 px-lg-5 my-5 brillos">
+         <img src="{{asset('assets/img/logo-kajeki.jpeg')}}" alt="Logo Kajeki" class="logo">
         <div class="text-center text-white">
-            <h1 class="display-4 fw-bolder">KAJEKI</h1>
+            <h1 class="display-4 fw-bolder titulo-brillante">KAJEKI</h1>
             <p class="lead fw-normal text-white mb-0">Tienda de productos cosméticos
             </p>
         </div>
     </div>
 </header>
+
+  <div class="imagen-izquierda">
+      <img src="{{asset('assets/img/mujer2.png')}}" alt="Mujer maquillándose">
+    </div>
+  </div>
+</section>
+
+<div class="imagen-derecha">
+      <img src="{{asset('assets/img/mujer.png')}}" alt="Mujer maquillándose">
+    </div>
+  </div>
+</section>
+
